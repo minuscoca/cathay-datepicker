7f91db1760e09b055e793e018fde11b7c1a682b2
Kevin <minuscoca@gmail.com>
2023-10-13 16:39:01 +0800

    start project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,29 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      backgroundColor: {
+        "btn-default": "rgb(var(--btn-background-default) / <alpha-value>)",
+        "btn-hover": "rgb(var(--btn-background-hover) / <alpha-value>)",
+        "btn-today": "rgb(var(--btn-background-today) / <alpha-value>)",
+        "btn-active": "rgb(var(--btn-background-active) / <alpha-value>)",
+        "btn-disabled": "rgb(var(--btn-background-disabled) / <alpha-value>)",
+      },
+      colors: {
+        "btn-default": "rgb(var(--btn-foreground-default) / <alpha-value>)",
+        "btn-hover": "rgb(var(--btn-foreground-hover) / <alpha-value>)",
+        "btn-today": "rgb(var(--btn-foreground-today) / <alpha-value>)",
+        "btn-active": "rgb(var(--btn-foreground-active) / <alpha-value>)",
+        "btn-disabled": "rgb(var(--btn-foreground-disabled) / <alpha-value>)",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
